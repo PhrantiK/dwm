2934252bc832b8ec30d22f42b24a20595b3d2368
PhrantiK <45719263+PhrantiK@users.noreply.github.com>
2021-09-23 16:36:08 +0800

    add dwmblocks folder

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,16 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"", "dwm_network",					5,		1}, */
+	{"", "dwm_cpu_temp",				        5,		8},
+	/* {"", "dwm_traffic",					5,		2}, */
+	{"", "dwm_uptime",					300,		3},
+	{"", "dwm_volume",					0,		4},
+	{"", "dwm_memory",					5,		5},
+	{"", "dwm_date",					60,		6},
+	{"", "dwm_clock",					60,		7},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "  ";
+static unsigned int delimLen = 5;
