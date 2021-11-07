16ec4f11d07965e8a89affc0b4e31cd616e26230
PhrantiK <45719263+PhrantiK@users.noreply.github.com>
2021-11-07 22:43:39 +0800

    updates

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,8 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"", "dwm_network",					5,		1}, */
 	{"", "dwm_cpu_temp",				        5,		8},
-	{"", "dwm_traffic",					4,		2},
-	{"", "dwm_updates",					300,		3},
+	/* {"", "dwm_traffic",					4,		2}, */
+	/* {"", "dwm_updates",					300,		3}, */
 	{"", "dwm_volume",					0,		4},
 	{"", "dwm_memory",					5,		5},
 	{"", "dwm_date",					60,		6},
